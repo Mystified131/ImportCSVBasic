96ff47ef59c9b0ddabeb11a4d87c6a9e80a67de4
mystified131 <mystifiedthomas@gmail.com>
2019-01-19 21:55:23 -0600

    Initial commit.

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using MVCApplication.ViewModels;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        internal static Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+
+
+        public IActionResult Index()
+        {
+
+ 
+
+            return View();
+
+
+        }
+
+        public IActionResult Values(string column)
+        {
+
+            ValuesViewModel valuesViewModel = new ValuesViewModel();
+
+
+            List<Dictionary<string, string>> elements = CSVData.FindAll();
+
+            valuesViewModel.elements = elements;
+
+            return View(valuesViewModel);
+        }
+
+
+        public IActionResult Results(string searchType, string searchTerm)
+
+        {
+
+
+            if (searchTerm != null)
+
+            {
+                List<Dictionary<string, string>> elements = CSVData.FindByValue(searchTerm);
+                ViewBag.elements = elements;
+                return View();
+
+
+            }
+                    
+
+            return Redirect("/");
+        }
+
+    }
+
+}
---
+++ MVCApplication/ViewModels/ValuesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.ViewModels
+{
+    public class ValuesViewModel
+    {
+        public List<Dictionary<string, string>> elements { get; set; }
+        public string column { get; set; }
+        public List<string> items { get; set; }
+    }
+}
